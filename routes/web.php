51bad5b2f1dcb4061a70c520f635d201586900b1
nirjhor703 <tahiyat.ahmed703@gmail.com>
2026-07-16 16:27:33 +0600

    Add admin authentication with Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
-use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/grey-stone');
+    return view('welcome');
 });
 
-Route::get('/{slug}', [BrandController::class, 'show'])
-    ->whereIn('slug', ['grey-stone', 'blue-shades', 'pink-touch'])
-    ->name('brand.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
