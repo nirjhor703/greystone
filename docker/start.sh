f932cb2e7e41699f6d4fff830a8c403f247eac7c
nirjhor703 <tahiyat.ahmed703@gmail.com>
2026-08-18 01:20:53 +0600

    Add Render deployment configuration

---
+++ docker/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cd /var/www/html
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php-fpm -D
+
+nginx -g "daemon off;"
